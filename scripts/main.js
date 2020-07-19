5446daa28bcdfe42bb9e5410fcbf5cce0e8cf785
PhantomBantam <lucnguyen1416@gmail.com>
2020-07-19 15:34:30 -0700

    minor comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 //remember to git commit every 1 hr or so of coding, its worth saving that stuff
 // if you've finished a big piece of the program thats good to commit too
 // make sure to always have useful git commit msgs
+
+//git status
+//git add [change name]
+//git commit -m "msg"
+//git push
+//git clone
+//git pull
+//git
+//cd..
+//cd [folder name]
